81000c0d429fcff7ee625be70c1198f5f4e964e5
Santtu S <Santtu.salonen@leanware.fi>
2021-12-05 16:10:41 +0200

    Correct test case id

--- src/test/toString.test.js
+++ src/test/toString.test.js
@@ -5,46 +5,46 @@ const expect = chai.expect
 
 describe("Unit tests of toString", () => {
 
-    it("SLI001: One parameter is given: a letter", () => {
+    it("TS001: One parameter is given: a letter", () => {
         expect(toString('a')).to.deep.equal("a")
     });
-    it("SLI002: One parameter is given: two letters", () => {
+    it("TS002: One parameter is given: two letters", () => {
         expect(toString('as')).to.deep.equal("as")
     });
-    it("SLI003: One parameter is given: a number", () => {
+    it("TS003: One parameter is given: a number", () => {
         expect(toString(1)).to.deep.equal('1')
     });
-    it("SLI004: One parameter is given: two numbers", () => {
+    it("TS004: One parameter is given: two numbers", () => {
         expect(toString(12)).to.deep.equal('12')
     });
-    it("SLI005: One parameter is given: two letters and two numbers", () => {
+    it("TS005: One parameter is given: two letters and two numbers", () => {
         expect(toString('aa12')).to.deep.equal('aa12')
     });
-    it("SLI006: One parameter is given: a symbol", () => {
+    it("TS006: One parameter is given: a symbol", () => {
         expect(toString('@')).to.deep.equal('@')
     });
-    it("SLI007: One parameter is given: an array: a number, a sumbol and a letter", () => {
+    it("TS007: One parameter is given: an array: a number, a symbol and a letter", () => {
         expect(toString([1, '@', 'a'])).to.deep.equal('1,@,a')
     });
-    it("SLI008: One parameter is given: null", () => {
+    it("TS008: One parameter is given: null", () => {
         expect(toString(null)).to.deep.equal('')
     });
-    it("SLI009: One parameter is given: an empty string", () => {
+    it("TS009: One parameter is given: an empty string", () => {
         expect(toString('')).to.deep.equal('')
     });
-    it("SLI0010: One parameter is given: undefined", () => {
+    it("TS0010: One parameter is given: undefined", () => {
         expect(toString(undefined)).to.deep.equal(undefined)
     });
-    it("SLI0011: One parameter is given: 5 letters", () => {
+    it("TS0011: One parameter is given: 5 letters", () => {
         expect(toString('aaaaa')).to.deep.equal("aaaaa")
     });
-    it("SLI0012: One parameter is given: 6 letters", () => {
+    it("TS0012: One parameter is given: 6 letters", () => {
         expect(toString('aaaaaa')).to.deep.equal("aaaaaa")
     });
-    it("SLI0013: One parameter is given: 10 letters", () => {
+    it("TS0013: One parameter is given: 10 letters", () => {
         expect(toString('aaaaaaaaaa')).to.deep.equal("aaaaaaaaaa")
     });
-    it("SLI0014: Parameter is missing", () => {
+    it("TS0014: Parameter is missing", () => {
         expect(toString()).to.deep.equal("")
     });
 
